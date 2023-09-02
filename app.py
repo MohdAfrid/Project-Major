1db6ca7934018076e94390c8c4e20199aa7171ba
MohdAfrid <83690734+MohdAfrid@users.noreply.github.com>
2023-09-03 00:17:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,38 +4,37 @@ import pandas as pd
 import pickle
 
 app = Flask(__name__)   
-model=pickle.load(open('ARSA.pkl','rb'))
+model=pickle.load(open('AmazonReview.pkl','rb'))
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-tfidf_vectorizer  = TfidfVectorizer(max_features=5000,ngram_range=(2,2))
-corpus=pd.read_csv('ARSAcorpus.csv')
+cv = TfidfVectorizer(max_features = 1500)
+corpus=pd.read_csv('AmazonCorpus.csv')
 corpus1=corpus['corpus'].tolist()
-X = tfidf_vectorizer.fit_transform(corpus1).toarray()
+corpus1 = [str(text) if pd.notna(text) else '' for text in corpus1]
+X = cv.fit_transform(corpus1).toarray()
 
 
 @app.route('/')
 def home():
-  
+
     return render_template("index.html")
 
-  
+
 @app.route('/predict',methods=['GET'])
 
 def predict():
-    
+
     text = (request.args.get('text'))
     text=[text]
-    input_data = tfidf_vectorizer.transform(text).toarray()
-    
-    
+    input_data = cv.transform(text).toarray()
+
+
     prediction = model.predict(input_data)
-    
-    if prediction == 2:
-      return render_template('index.html', prediction_text='Negative')
-    elif prediction == 1:
-      return render_template('index.html', prediction_text='Neutral')     
-    else:
+    if prediction == 1:
       return render_template('index.html', prediction_text='Positive')
+
+    else:
+      return render_template('index.html', prediction_text='Negative')
 #------------------------------About us-------------------------------------------
 @app.route('/aboutusnew')
 def aboutusnew():
